dd42c323a1f429dab12b995f9c153febb40ab126
Djalma Beraldo <djalma.batista@indt.org.br>
2019-08-16 10:20:08 -0400

    First commit

---
+++ frontend/static/js/restfull.js
@@ -0,0 +1,126 @@
+const url='http://127.0.0.1:5000/livro';
+var objId = null;
+
+function carregarDados(){
+
+    fetch(url,{
+        method: 'GET'
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response) { 
+        preencherTabela(response);
+    })
+    .catch(function(err) { console.error(err); });
+}
+
+function carregarFormularioCadastroEdicao(){
+    window.location = url + '/criar';
+}
+
+function preencherTabela(livros){
+    var rows = "";
+    for (var key in livros){
+        var contentRow = "<tr><td>"+livros[key].titulo+"</td>"
+        + "<td>"+livros[key].autor+"</td>"
+        +"<td>"+livros[key].isbn+"</td>"
+        +"<td>"+livros[key].editora+"</td>"
+        +"<td>"+livros[key].edicao+"</td>"
+        +"<td>R$ "+livros[key].preco+",00</td>"
+        +"<td><input type='button' value='Editar' onclick='editarLivro("+livros[key].id+")'>"
+        +"<input type='button' value='Excluir' onclick='excluirLivro("+livros[key].id+")'></td>"
+        +"</tr>"
+        rows = rows + contentRow;
+    }
+
+    document.getElementById("content-row").innerHTML = rows;
+}
+
+function cancelarAdicionar(){
+    document.getElementById("form-add").style.display='none';
+    document.getElementById("table-area").style.display='block';
+}
+
+
+function cadastrarOuEditarLivro(){
+
+    //Pega os valores do formulário
+    var formData = {"titulo": document.getElementById("titulo").value,
+    "autor": document.getElementById("autor").value,
+    "isbn": document.getElementById("isbn").value,
+    "preco": document.getElementById("preco").value,
+    "edicao": document.getElementById("edicao").value,
+    "editora": document.getElementById("editora").value,
+    "id": objId};
+
+    var headers = new Headers();
+    headers.append("Content-type","application/json")
+
+    console.log(objId);
+
+    fetch(url,{
+        method: (formData.id === null ? 'POST':'PUT'),
+        body: JSON.stringify(formData),
+        headers: headers
+    })
+    .then(function(response) { 
+        response.text()
+        .then(function(result){ 
+            document.getElementById("table-area").style.display='block';
+            document.getElementById("form-add").style.display='none';
+            limparTabela();
+            carregarDados();
+        }) 
+    })
+    .catch(function(err) { console.error(err); });
+}
+
+function editarLivro(id){
+
+    //Carrega formulário para edição
+    carregarFormularioCadastroEdicao();
+    
+    //Busca dados do livro
+    fetch(url+'/'+id+'/',{
+        method: 'GET'
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response) { 
+        for(key in response){
+            if(key === 'id'){
+                objId = response[key];
+            }else{
+                 //Atribui valores ao campos
+                 console.log(key);
+                document.getElementById(key).value = response[key];
+            }
+        }
+    })
+    .catch(function(err) { console.error(err); });
+}
+
+function excluirLivro(id){
+    fetch(url+'/'+id+'/',{
+        method: 'DELETE'
+    })
+    .then(function(response) { 
+        response.text()
+        .then(function(result){ 
+            carregarDados();
+        }) 
+    })
+    .catch(function(err) { console.error(err); });
+}
+
+function limparTabela(){
+    document.getElementById("titulo").value = '';
+    document.getElementById("autor").value ='';
+    document.getElementById("isbn").value = '';
+    document.getElementById("preco").value = '';
+    document.getElementById("edicao").value ='';
+    document.getElementById("editora").value = '';
+    objId = null;
+}
---
+++ store.py
@@ -0,0 +1,89 @@
+
+from flask import Flask, request, jsonify, g, Response, render_template
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow 
+import json
+import os
+
+# Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
+
+# book Class/Model
+class Book(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  author = db.Column(db.String(100))
+  title = db.Column(db.String(200))
+
+  def __init__(self, author, title):
+    self.author = author
+    self.title = title
+
+# book Schema
+class bookSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'author', 'title')
+
+# Init schema
+book_schema = bookSchema(strict=True)
+books_schema = bookSchema(many=True, strict=True)
+
+# Create a book
+@app.route('/book', methods=['POST'])
+def add_book():
+  author = request.json['author']
+  title = request.json['title']
+
+  new_book = Book(author, title)
+
+  db.session.add(new_book)
+  db.session.commit()
+
+  return book_schema.jsonify(new_book)
+
+# Get All books
+@app.route('/book', methods=['GET'])
+def get_books():
+  all_books = Book.query.all()
+  result = books_schema.dump(all_books)
+  return jsonify(result.data)
+
+# Get Single books
+@app.route('/book/<id>', methods=['GET'])
+def get_book(id):
+  book = book.query.get(id)
+  return book_schema.jsonify(book)
+
+# Update a book
+@app.route('/book/<id>', methods=['PUT'])
+def update_book(id):
+  book = book.query.get(id)
+
+  author = request.json['author']
+  title = request.json['title']
+
+  book.author = author
+  book.title = title
+
+  db.session.commit()
+
+  return book_schema.jsonify(book)
+
+# Delete book
+@app.route('/book/<id>', methods=['DELETE'])
+def delete_book(id):
+  book = book.query.get(id)
+  db.session.delete(book)
+  db.session.commit()
+
+  return book_schema.jsonify(book)
+
+if __name__ == "__main__":
+    app.run(debug=True)
